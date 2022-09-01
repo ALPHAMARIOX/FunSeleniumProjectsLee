f57a5c060b774418415852bddcacbfe5f2540a85
Lee Gordon Joseph Harrold <alphamariox@gmail.com>
2022-09-01 04:40:27 -0600

    Updated the Twitter Login

--- TwitterLogin.py
+++ TwitterLogin.py
@@ -7,16 +7,12 @@ import time
 service_obj = Service("C:\\Users\\AlphaMK\\Downloads\\chromedriver")
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://twitter.com/i/flow/login")
+time.sleep(2)
 
-driver.find_element(By.XPATH, '//input[@name="text"]').send_keys("Username")
-driver.find_element(By.XPATH, '//input[@name="text"]').send_keys(Keys.ENTER)
+driver.find_element(By.CSS_SELECTOR, "input[name='text']").send_keys("Username")
+driver.find_element(By.CSS_SELECTOR, "input[name='text']").send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-driver.find_element(By.XPATH, "//input[name='password']").send_keys("Password")
-driver.find_element(By.XPATH, "//input[name='password']").send_keys(Keys.ENTER)
-
-
-# Since my account has MFA (Multi-Factor Authentication, I will be notified on my phone about the confirmation code.
-# Base code for this project is found on https://www.geeksforgeeks.org/login-twitter-using-python-selenium/
-# Additional adjustments were made due to Twitter changing their website's front end and back end frequently, as most companies do.
+driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys("Password")
+driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys(Keys.ENTER)
